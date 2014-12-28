e89bc7b334be9909566f84af615224f5e3505e3c
Fanxi Zeng <fxzeng@yahoo-inc.com>
2014-12-28 19:31:27 +0800

    fetch bus line data from 8684

--- data/sh/get_bus_data.sh
+++ data/sh/get_bus_data.sh
@@ -1,4 +1,4 @@
 for((line=1;line<1000;line++));
 do
-    curl "http://beijing.8684.cn/so.php?k=pp&q=${line}" > beijing/${line}.html;
+    curl -o beijing/${line}.html -L --max-redirs 100 "http://beijing.8684.cn/so.php?k=pp&q=${line}";
 done
