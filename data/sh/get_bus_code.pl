18ea89674cd4bd0d5853988d2d8cd2747c7efe93
fxzeng <fxzeng@UbuntuFXZ.(none)>
2014-12-28 21:57:40 +0800

    add some comments

--- data/sh/get_bus_code.pl
+++ data/sh/get_bus_code.pl
@@ -1,7 +1,14 @@
 package BusHelper;
 
+# when 980.html returns a bunch of 980-like bus lines
+# return 
+# {
+#    xa_adfa778ab => 980lu
+#    xb_adf7668ba => zhuan980
+# }
 sub get_buses {
   my $bus_code = shift;
+
   my $file = $bus_code . ".html";
   my $content = `cat $file`;
   chomp $content;
@@ -19,6 +26,36 @@ sub get_buses {
 }
 
 
+# given a bus line number, such as 980, we get the file 980.html
+# return whether it's a page including all stations of line 980
+# or it return a page including a bunches of 980-like lines
+sub is_bunch {
+  my $bus_code = shift;
+
+  my $file = $bus_code . ".html";
+  my $content = `cat $file`;
+  chomp $content;
+
+#  my $regex = '<div class="sr-hd">ËÑË÷¡°' . $bus_code . '¡±½á¹û</div>';
+  my $regex = 'ËÑË÷¡°' . $bus_code . '¡±½á¹û';
+
+  print $regex . "\n";
+  if($content =~ /$regex/) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+my $is_bunch = is_bunch(980);
+if($is_bunch) {
+  print "980.html is a bunch file\n";
+} else {
+  print "980.html is not a bunch file\n";
+}
+
+
+print "980.html's content is:\n";
 my $buses = get_buses(980);
 foreach my $key (keys %$buses) {
   print "$key   compare   $buses->{$key}\n";
